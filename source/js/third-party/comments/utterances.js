b513045e242abc303064018f0efc3ba830cefef9
NekoDaemon <Gutongzhou@gmail.com>
2022-01-30 23:42:56 +0800

    implement

--- source/js/third-party/comments/utterances.js
+++ source/js/third-party/comments/utterances.js
@@ -4,13 +4,19 @@ document.addEventListener('page:loaded', () => {
   if (!CONFIG.page.comments) return;
 
   NexT.utils.loadComments('.utterances-container')
-    .then(() => NexT.utils.getScript('https://utteranc.es/client.js', {
+    .then(() => NexT.utils.getScript('https://giscus.app/client.js', {
       attributes: {
         async       : true,
         crossOrigin : 'anonymous',
-        'repo'      : CONFIG.utterances.repo,
-        'issue-term': CONFIG.utterances.issue_term,
-        'theme'     : CONFIG.utterances.theme
+        'data-repo' : "Tonny-Gu/blog_comments",
+        'data-repo-id' : "MDEwOlJlcG9zaXRvcnkyMzMzNTc0OTM=",
+        'data-category-id' : "DIC_kwDODejAtc4CA6hf",
+        'data-mapping' : "pathname",
+        'data-reactions-enabled' : "1",
+        'data-emit-metadata' : "0",
+        'data-input-position' : "bottom",
+        'data-theme' : "light",
+        'data-lang' : "en"
       },
       parentNode: document.querySelector('.utterances-container')
     }));
